83d09baa345908cafbf49bf301aa471480a430df
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2024-08-28 09:04:05 +0530

    Best practices

--- 01reactbasics/src/App.jsx
+++ 01reactbasics/src/App.jsx
@@ -1,11 +1,17 @@
-import Temp from './Temp.jsx' 
+import Temp from "./Temp.jsx";
 
 function App() {
   // return <h1>Hello React with Vite ~ AKS</h1>;
-  return (
-    <Temp />
-  );
+  // return (
+  //   <Temp />
+  // );
 
+  return (
+    <>
+      <Temp />
+      <h1> Fragment returned </h1>
+    </>
+  );
 }
 
 export default App;
