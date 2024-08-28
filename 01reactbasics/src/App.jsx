0d5f274434fb2bf564614d78b81ef0e6071173b8
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2024-08-28 08:54:09 +0530

    nested rendering

--- 01reactbasics/src/App.jsx
+++ 01reactbasics/src/App.jsx
@@ -1,5 +1,11 @@
+import Temp from './Temp.jsx' 
+
 function App() {
-  return <h1>Hello React with Vite ~ AKS</h1>;
+  // return <h1>Hello React with Vite ~ AKS</h1>;
+  return (
+    <Temp />
+  );
+
 }
 
 export default App;
