b6999a39ac75fbe13b555d9b585ddd7a42b46b81
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2024-08-30 08:08:53 +0530

    hooks

--- 03hooks_project_counter/src/App.jsx
+++ 03hooks_project_counter/src/App.jsx
@@ -1,7 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 //-> below counter variable will not increamnet every where so here comes the hook concepts
 // function App() {
 
@@ -25,27 +25,29 @@ import './App.css'
 //   )
 // }
 
-
 function App() {
-  const [counter, setCounter] = useState(5)
-
+  const [counter, setCounter] = useState(5);
 
   const addValue = () => {
-    setCounter(counter + 1)
-  }
+    setCounter(counter + 1);
+  };
   const subValue = () => {
-    setCounter(counter - 1)
-  }
+    setCounter(counter - 1);
+  };
 
   return (
     <>
       <h1>React With AKS!</h1>
       <h3>Counter Value: {counter}</h3>
-      <button id="add" onClick={addValue}>Increase</button>
+      <button id="add" onClick={addValue}>
+        Increase
+      </button>
       <br />
-      <button id="sub" onClick={subValue}>Decrease</button>
+      <button id="sub" onClick={subValue}>
+        Decrease
+      </button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
