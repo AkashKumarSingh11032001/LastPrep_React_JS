4fec5da3a3a0bbd94efc493cf625784a449a9186
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2024-08-31 21:58:31 +0530

    props

--- 04props_tailwind/src/App.jsx
+++ 04props_tailwind/src/App.jsx
@@ -1,40 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Card from "./components/Card.jsx"
 
-function App() { 
-  const [count, setCount] = useState(0)
+function App() {
+
+  let info = {
+    name:"Akash",
+    desc:"Great Coder"
+  }
+
+  let infoArray = [1,2,3]
 
   return (
     <>
-      <h1 className='bg-green-400'>Tailwind Test</h1>
-      <figure className="bg-slate-100 rounded-xl p-8 dark:bg-slate-800">
-      <img
-    src="https://images.unsplash.com/photo-1546961329-78bef0414d7c?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHVzZXJ8ZW58MHx8MHx8&amp;auto=format&amp;fit=crop&amp;w=400&amp;q=60"
-    alt="AirMax Pro"
-    className="z-0 h-full w-full rounded-md object-cover"
-  />
-  <div className="pt-6 space-y-4">
-    <blockquote>
-      <p className="text-lg font-medium">
-        “Tailwind CSS is the only framework that I've seen scale
-        on large teams. It’s easy to customize, adapts to any design,
-        and the build size is tiny.”
-      </p>
-    </blockquote>
-    <figcaption className="font-medium">
-      <div className="text-sky-500 dark:text-sky-400">
-        Sarah Dayan
-      </div>
-      <div>
-        Staff Engineer, Algolia
-      </div>
-    </figcaption>
-  </div>
-</figure>
+      <Card name="Akash" infoX={info} newArray={infoArray} />
+      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
