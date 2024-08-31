b1abc1e6611d4efc7dd7ec76f8b77de505ff503a
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2024-08-31 22:00:46 +0530

    props handling

--- 04props_tailwind/src/components/Card.jsx
+++ 04props_tailwind/src/components/Card.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-function Card(props) {
+function Card(props) { //-> {name,infoX,newArray}
     console.log("props", props);
     
   return (
@@ -13,9 +13,9 @@ function Card(props) {
         />
         <div className="absolute inset-0 bg-gradient-to-t from-gray-900 to-transparent"></div>
         <div className="absolute bottom-4 left-4 text-left">
-          <h1 className="text-lg font-semibold text-white">Delba</h1>
+          <h1 className="text-lg font-semibold text-white">{props.name}</h1>
           <p className="mt-2 text-sm text-gray-300">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
+            {props.infoX.desc} Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
             debitis?
           </p>
           <button className="mt-2 inline-flex cursor-pointer items-center text-sm font-semibold text-white">
