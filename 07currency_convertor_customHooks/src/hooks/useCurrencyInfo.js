5205f814820a55b18fb4bdf542e5b15b27ad6295
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2024-09-07 11:21:01 +0530

    custom hooks

--- 07currency_convertor_customHooks/src/hooks/useCurrencyInfo.js
+++ 07currency_convertor_customHooks/src/hooks/useCurrencyInfo.js
@@ -4,25 +4,21 @@
 
 import { useEffect, useState } from "react";
 
-function useCurrencyInfo(currency){
-    // const [currencyInfo, setCurrencyInfo] = useState(null);
-    // useEffect(()=>{
-    //     fetch(`https://api.exchangerate.host/latest?base=${currency}`)
-    //     .then(res=>res.json())
-    //     .then(data=>{
-    //         setCurrencyInfo(data.rates);
-    //     })
-    // },[currency])
-    // return currencyInfo;
+function useCurrencyInfo(currency) {
+  const [currencyInfo, setCurrencyInfo] = useState(null);
+  useEffect(() => {
+    fetch(
+      `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`
+    ) // json is in string so covert to json
+      .then((res) => res.json)
+      .then((data) => {
+        setCurrencyInfo(data[currency]);
+      });
+  }, [currencyInfo]);
 
-    const [currencyInfo, setCurrencyInfo] = useState(null);
-    useEffect(()=>{
-        fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`) // json is in string so covert to json
-        .then(res => res.json)
-        .then(data=>{
-            setCurrencyInfo(data[currency]);
-        })
-    }, [currencyInfo])
+  return currencyInfo;
 }
 
-//-> Explain useEffect() in lamen term or easy to understand term? 
+export default useCurrencyInfo;
+
+//-> Explain useEffect() in lamen term or easy to understand term?
