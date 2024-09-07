64bbeec7c0519cf33205b8919594d108bc35407f
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2024-09-07 14:16:14 +0530

    Currency Convertor

--- 07currency_convertor_customHooks/src/components/InputBox.jsx
+++ 07currency_convertor_customHooks/src/components/InputBox.jsx
@@ -40,3 +40,4 @@ function InputBox({
 }
 
 export default InputBox;
+ 
--- 07currency_convertor_customHooks/src/hooks/useCurrencyInfo.js
+++ 07currency_convertor_customHooks/src/hooks/useCurrencyInfo.js
@@ -5,15 +5,13 @@
 import { useEffect, useState } from "react";
 
 function useCurrencyInfo(currency) {
-  const [currencyInfo, setCurrencyInfo] = useState(null);
+  const [currencyInfo, setCurrencyInfo] = useState({});
   useEffect(() => {
     fetch(
-      `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`
+      `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@2024-03-06/v1/currencies/${currency}.json`
     ) // json is in string so covert to json
-      .then((res) => res.json)
-      .then((data) => {
-        setCurrencyInfo(data[currency]);
-      });
+      .then((res) => res.json())
+      .then((data) => setCurrencyInfo(data[currency]));
   }, [currencyInfo]);
 
   return currencyInfo;
