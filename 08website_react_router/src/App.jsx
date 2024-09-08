aeeeb521b50f38bcfc8e3cb3976c002780e043ac
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2024-09-08 18:02:47 +0530

    website setup

--- 08website_react_router/src/App.jsx
+++ 08website_react_router/src/App.jsx
@@ -1,21 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Header from './components/Header/Header'
-import Footer from './components/Footer/Footer'
-import Home from './components/Home/Home'
-import { Outlet } from 'react-router-dom'
+import "./App.css";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+
+import { Outlet } from "react-router-dom";
 
 function App() {
-
   return (
     <>
       <Header />
       <Outlet />
       <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
