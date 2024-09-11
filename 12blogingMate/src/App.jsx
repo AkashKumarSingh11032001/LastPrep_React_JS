539af59a23f1c2bac3421543c7f7897b3078892c
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2024-09-11 14:30:05 +0530

    setting env

--- 12blogingMate/src/App.jsx
+++ 12blogingMate/src/App.jsx
@@ -1,12 +1,15 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
+  // console.log(process.env.REACT_APP_APPWRITE_URL); --> this is usefull when react app created using create-react-app [always read environment documentation]
+  console.log(import.meta.env.VITE_APPWRITE_URL);
+  
+  
   return (
     <>
       <h1>Blogging Application</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
