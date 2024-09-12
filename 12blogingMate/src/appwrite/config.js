2a83a549c051beaed30f341cc98f43b174466359
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2024-09-12 07:59:29 +0530

    databases service creation

--- 12blogingMate/src/appwrite/config.js
+++ 12blogingMate/src/appwrite/config.js
@@ -39,25 +39,64 @@ export class Service {
     }
   }
 
-  async updatePost(slug, {title, content, featuredImages, status}){
-    try{
-        const res= await this.databases.updateDocument(
-            conf.APPWRITE_DATABASE_ID,
-            conf.APPWRITE_COLLECTION_ID,
-            slug,
-            {
-                title,
-                content,
-                featuredImages,
-                status,
-            }
-        )
-        return res;
-
-    }catch(error){
+  async updatePost(slug, { title, content, featuredImages, status }) {
+    try {
+      const res = await this.databases.updateDocument(
+        conf.APPWRITE_DATABASE_ID,
+        conf.APPWRITE_COLLECTION_ID,
+        slug,
+        {
+          title,
+          content,
+          featuredImages,
+          status,
+        }
+      );
+      return res;
+    } catch (error) {
       console.log("Appwrite service :: updatePost :: error", error);
     }
+  }
 
+  async deletePost(slug) {
+    try {
+      const res = await this.databases.deleteDocument(
+        conf.APPWRITE_DATABASE_ID,
+        conf.APPWRITE_COLLECTION_ID,
+        slug
+      );
+      return true;
+    } catch (error) {
+      console.log("Appwrite service :: deletePost :: error", error);
+    }
+    return false;
+  }
+
+  async getPost(slug) {
+    try {
+      const res = await this.databases.getDocument(
+        conf.APPWRITE_DATABASE_ID,
+        conf.APPWRITE_COLLECTION_ID,
+        slug
+      );
+      return res;
+    } catch (error) {
+      console.log("Appwrite service :: getPost :: error", error);
+    }
+  }
+
+  async getPosts() {
+    try {
+      const res = await this.databases.listDocuments(
+        conf.APPWRITE_DATABASE_ID,
+        conf.APPWRITE_COLLECTION_ID,
+        [Query.equal("status","active")]
+      );
+      return res;
+    } catch (error) {
+      console.log("Appwrite service :: getAllPost :: error", error);
+      return false;
+    }
   }
 }
 
