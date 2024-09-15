dcfb596f9f9022b573c827b74ef9d9ecef8aff01
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2024-09-15 11:00:49 +0530

    header

--- 12blogingMate/src/components/Header/LogoutBtn.jsx
+++ 12blogingMate/src/components/Header/LogoutBtn.jsx
@@ -1,17 +1,23 @@
-import React from 'react'
-import {useDispatch} from 'react-redux'
-import authService from '../../appwrite/auth'
-import {logout} from '../../store/authSlice'
+import React from "react";
+import { useDispatch } from "react-redux";
+import authService from "../../appwrite/auth";
+import { logout } from "../../store/authSlice";
 
 const LogoutBtn = () => {
-    const dispatch = useDispatch()
-    const handleLogout = async () => {
-        await authService.logout()
-        dispatch(logout())
-    }
+  const dispatch = useDispatch();
+  const logoutHandler = () => {
+    authService.logout().then(() => {
+      dispatch(logout());
+    });
+  };
   return (
-    <div>LogoutBtn</div>
-  )
-}
+    <div
+      className="inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full"
+      onClick={logoutHandler}
+    >
+      LogoutBtn
+    </div>
+  );
+};
 
-export default LogoutBtn
+export default LogoutBtn;
